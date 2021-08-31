824492021ec624693f8c140286f730166a793d8f
一u一风荷举 <wangzhen0518@126.com>
2021-08-31 18:05:54 +0800

    2021/08/31 C++ Primer Plus

---
+++ C++ Primer Plus Test/Chapter11/11-9-5/Stone.h
@@ -0,0 +1,36 @@
+#ifndef STONEWT_H_
+#define STONEWT_H_
+#include <iostream>
+class Stonewt
+{
+private:
+    enum
+    {
+        Lbs_per_stn = 14
+    }; // pounds per stone
+    enum Mode
+    {
+        INT,
+        FLOAT
+    };
+    int stone;       // whole stones
+    double pds_left; // fractional pounds
+    double pounds;   // entire weight in pounds
+    Mode mode;
+
+public:
+    Stonewt(double lbs);          // construct from double pounds
+    Stonewt(int stn, double lbs); // construct from stone, lbs
+    Stonewt();                    // default constructor
+    ~Stonewt();
+    void setINT() { mode = INT; }
+    void setFLOAT() { mode = FLOAT; }
+    Stonewt operator+(const Stonewt &s) const;
+    Stonewt operator-(const Stonewt &s) const;
+    Stonewt operator*(double n) const;
+    operator int() const;
+    operator double() const;
+    friend Stonewt operator*(double n, const Stonewt &s);
+    friend std::ostream &operator<<(std::ostream &os, const Stonewt &s);
+};
+#endif
